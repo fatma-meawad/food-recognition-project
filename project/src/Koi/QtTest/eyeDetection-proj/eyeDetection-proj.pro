58b2743e175269a497d3a05164d82aa12cf7ba14
Simon.Elgland <Simon.Elgland@967ed32d-291c-a2c0-ab79-827932dd879b>
2012-04-24 18:54:01 +0000

    CPP version of facedetect

---
+++ project/src/Koi/QtTest/eyeDetection-proj/eyeDetection-proj.pro
@@ -0,0 +1,28 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2012-04-24T19:59:41
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = eyeDetection-proj
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
+
+unix:!macx:!symbian: LIBS += -L$$PWD/../../../../usr/lib/ -lopencv_core -lopencv_highgui -lopencv_imgproc -lopencv_objdetect
+
+INCLUDEPATH += $$PWD/../../../../usr/include
+DEPENDPATH += $$PWD/../../../../usr/include
+
+unix:!macx:!symbian: LIBS += -L$$PWD/../../../../usr/local/lib/ -lopencv_core -lopencv_highgui -lopencv_imgproc -lopencv_objdetect
+
+INCLUDEPATH += $$PWD/../../../../usr/local/include
+DEPENDPATH += $$PWD/../../../../usr/local/include
---
+++ project/src/Koi/QtTest/eyeDetection-proj/main.cpp
@@ -0,0 +1,86 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <math.h>
+#include <float.h>
+#include <limits.h>
+#include <time.h>
+#include <ctype.h>
+
+// Create memory for calculations
+static CvMemStorage* storage = 0;
+
+int main( int argc, char** argv )
+{
+    /**
+      * Initiera class (madde)
+      *
+      *
+      **/
+
+    // Structure for getting video from camera or avi
+    CvCapture* capture = 0;
+
+    // Images to capture the frame from video or camera or from file
+    IplImage *frame, *frame_copy = 0;
+
+    // Allocate the memory storage
+    storage = cvCreateMemStorage(0);
+
+    // Add path to see video, default is cam
+    if( argc > 1 )
+        capture = cvCaptureFromAVI( argv[1] );
+    else
+        capture = cvCaptureFromCAM( CV_CAP_ANY );
+
+    // Create a new named window with title: result
+    cvNamedWindow( "result", 1 );
+
+    // If loaded succesfully, then:
+    if( capture )
+    {
+        // Capture from the camera.
+        for(;;)
+        {
+            // Capture the frame and load it in IplImage
+            if( !cvGrabFrame( capture ))
+                break;
+            frame = cvRetrieveFrame( capture );
+
+            // If the frame does not exist, quit the loop
+            if( !frame )
+                break;
+
+            // Allocate framecopy as the same size of the frame
+            if( !frame_copy )
+                frame_copy = cvCreateImage( cvSize(frame->width,frame->height), IPL_DEPTH_8U, frame->nChannels );
+
+            // Check the origin of image. If top left, copy the image frame to frame_copy.
+            if( frame->origin == IPL_ORIGIN_TL )
+                cvCopy( frame, frame_copy, 0 );
+            // Else flip and copy the image
+            else
+                cvFlip( frame, frame_copy, 0 );
+
+             /**
+              *
+              *     Tillkalla era funktioner här
+              *
+              *
+              **/
+
+            // Wait for a while before proceeding to the next frame
+            if( cvWaitKey( 10 ) >= 0 )
+                break;
+        }
+
+        // Release the images, and capture memory
+        cvReleaseImage( &frame_copy );
+        cvReleaseCapture( &capture );
+    }
+    else
+        return -3;
+}
