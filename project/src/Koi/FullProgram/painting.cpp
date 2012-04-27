5478fcf8267d0fbd87bc40ab0ce38f302a1fac05
Simon.Elgland <Simon.Elgland@967ed32d-291c-a2c0-ab79-827932dd879b>
2012-04-27 10:53:07 +0000

    Paint draft

--- project/src/Koi/FullProgram/painting.cpp
+++ project/src/Koi/FullProgram/painting.cpp
@@ -3,3 +3,17 @@
 Painting::Painting()
 {
 }
+// Draw haircross
+IplImage* Painting::drawHairCross(IplImage* img, CvPoint p){
+    cvLine(img,cvPoint(p.x-5,p.y),cvPoint(p.x+5,p.y),cvScalar(100,0,100));
+    cvLine(img,cvPoint(p.x,p.y-5),cvPoint(p.x,p.y+5),cvScalar(100,0,100));
+
+    return img;
+}
+
+// Draw rectangle
+IplImage* Painting::drawRect(IplImage * img, CvRect rect){
+    cvRectangle(img,cvPoint(rect.x,rect.y),cvPoint(rect.x+rect.width,rect.y+rect.height),cvScalar(255,0,0));
+
+    return img;
+}
--- project/src/Koi/FullProgram/painting.h
+++ project/src/Koi/FullProgram/painting.h
@@ -7,6 +7,12 @@ class Painting
 {
 public:
     Painting();
+
+    IplImage* drawHairCross(IplImage*, CvPoint);
+    IplImage* drawRect(IplImage*, CvRect);
+    IplImage* drawCircle(IplImage*, CvPoint);
+
+    void drawGraph();
 };
 
 #endif // PAINTING_H
