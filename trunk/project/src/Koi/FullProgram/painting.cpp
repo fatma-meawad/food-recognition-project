5da00bb081a6f9b633aa3c665d2faa417a8deb04
Simon.Elgland <Simon.Elgland@967ed32d-291c-a2c0-ab79-827932dd879b>
2012-04-27 14:04:47 +0000

    Paint update

--- trunk/project/src/Koi/FullProgram/painting.cpp
+++ trunk/project/src/Koi/FullProgram/painting.cpp
@@ -5,15 +5,24 @@ Painting::Painting()
 }
 // Draw haircross
 IplImage* Painting::drawHairCross(IplImage* img, CvPoint p){
-    cvLine(img,cvPoint(p.x-5,p.y),cvPoint(p.x+5,p.y),cvScalar(100,0,100));
-    cvLine(img,cvPoint(p.x,p.y-5),cvPoint(p.x,p.y+5),cvScalar(100,0,100));
+    cvLine(img,cvPoint(p.x-5,p.y),cvPoint(p.x+5,p.y),cvScalar(100,0,100), 2);
+    cvLine(img,cvPoint(p.x,p.y-5),cvPoint(p.x,p.y+5),cvScalar(100,0,100), 2);
 
     return img;
 }
 
 // Draw rectangle
 IplImage* Painting::drawRect(IplImage * img, CvRect rect){
-    cvRectangle(img,cvPoint(rect.x,rect.y),cvPoint(rect.x+rect.width,rect.y+rect.height),cvScalar(255,0,0));
+    cvRectangleR(img,rect,cvScalar(255,0,0), 5);
+
+    return img;
+}
+
+IplImage* Painting::drawFullFace(IplImage * img, Facefeatures * ff)
+{
+    drawRect(img, ff->mFace);
+    drawHairCross(img, ff->center(ff->mLeftEye));
+    drawHairCross(img, ff->center(ff->mRightEye));
 
     return img;
 }
