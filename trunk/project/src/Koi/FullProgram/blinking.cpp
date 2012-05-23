e0f26ee4c904a8a1f4c2f181c9afc73f88d5f27d
Simon.Elgland <Simon.Elgland@967ed32d-291c-a2c0-ab79-827932dd879b>
2012-05-23 13:17:19 +0000

    Blinking update, next step, test on other people

--- trunk/project/src/Koi/FullProgram/blinking.cpp
+++ trunk/project/src/Koi/FullProgram/blinking.cpp
@@ -4,6 +4,7 @@
 
 int avg = samples;
 double avgMass = 0;
+double openLen = 0;
 CvPoint avgCenter = cvPoint(0,0);
 
 int Blinking::Init()
@@ -27,35 +28,89 @@ bool CalcPixels(IplImage * inputImage)
     if(avg > 0)
     {
         double maxMass = mass;
+
         for(int i = 0; i < (input->width - eX); i++)
         {
+            int closeX = input->width;
+
             for(int j = 0; j < (input->height - eY); j++)
             {
+                int closeY = input->height;
                 mass = 0;
                 for(int k = j; k < j + eY; k++)
                     for(int l = i; l < i + eX; l++)
                     {
                         pixel = cvGetAt(input,k,l);
                         if(pixel.val[0] != 255)
+                        {
                             mass++;
+                            if(l < closeX)
+                                closeX = l;
+                            if(k < closeY)
+                                closeY = k;
+                        }
                     }
                 if(mass > maxMass)
                 {
                     massCenter = cvPoint(i,j); //std::cout << massCenter.x << "," << massCenter.y << ", " << mass << ",   " << input->width << "," <<input->height << std::endl;
                     maxMass = mass;
                 }
+
+                if(closeY - 1 > i && closeY != input->height)
+                    i = closeY - 1;
             }
+
+            if(closeX - 1 > i && closeX != input->width)
+                i = closeX - 1;
+
+        }
+
+        vector<CvPoint> blackPixels;
+
+        for(int i = 0; i < input->width; i++)
+        {
+            for(int j = 0; j < input->height; j++)
+            {
+                pixel = cvGetAt(input,j,i);
+                if(pixel.val[0] != 255)
+                    blackPixels.push_back(cvPoint(i,j));
+            }
+        }
+
+        double avgLen = 0;
+        if(blackPixels.size())
+        {
+
+            // Calculate center
+            int avgX = 0;
+            int avgY = 0;
+            for(unsigned int i = 0; i < blackPixels.size(); i++)
+            {
+                avgX += blackPixels.at(i).x;
+                avgY += blackPixels.at(i).y;
+            }
+            avgX = avgX/blackPixels.size();
+            avgY = avgY/blackPixels.size();
+
+            // Calculate average distance to from dark pixel to center
+            for(unsigned int i = 0; i < blackPixels.size(); i++)
+            {
+                avgLen += sqrt((avgX - blackPixels.at(i).x)*(avgX - blackPixels.at(i).x) + (avgY - blackPixels.at(i).y)*(avgY - blackPixels.at(i).y));
+            }
+            avgLen = avgLen/blackPixels.size();
         }
 
         if(avg == 1)
         {
             avgCenter = cvPoint((massCenter.x + avgCenter.x)/samples, (massCenter.y + avgCenter.y)/samples);
             avgMass = (avgMass + maxMass)/samples;
+            openLen = (openLen + avgLen)/samples;
         }
         else
         {
             avgCenter = cvPoint(massCenter.x + avgCenter.x, massCenter.y + avgCenter.y);
             avgMass += maxMass;
+            openLen += avgLen;
         }
 
         avg--;
@@ -77,8 +132,55 @@ bool CalcPixels(IplImage * inputImage)
                     mass++;
             }
 
+        vector<CvPoint> blackPixels;
+
+        for(int i = 0; i < input->width; i++)
+        {
+            for(int j = 0; j < input->height; j++)
+            {
+                pixel = cvGetAt(input,j,i);
+                if(pixel.val[0] != 255)
+                    blackPixels.push_back(cvPoint(i,j));
+            }
+        }
+
+        /*for(int k = avgCenter.y; k < avgCenter.y + eY; k++)
+            for(int l = avgCenter.x; l < avgCenter.x + eX; l++)
+            {
+                pixel = cvGetAt(input,k,l);
+                if(pixel.val[0] != 255)
+                    blackPixels.push_back(cvPoint(l,k));
+            }*/
+
+        if(blackPixels.size())
+        {
+
+            // Calculate center
+            int avgX = 0;
+            int avgY = 0;
+            for(unsigned int i = 0; i < blackPixels.size(); i++)
+            {
+                avgX += blackPixels.at(i).x;
+                avgY += blackPixels.at(i).y;
+            }
+            avgX = avgX/blackPixels.size();
+            avgY = avgY/blackPixels.size();
+
+            // Calculate average distance to from dark pixel to center
+            double avgLen = 0;
+            for(unsigned int i = 0; i < blackPixels.size(); i++)
+            {
+                avgLen += sqrt((avgX - blackPixels.at(i).x)*(avgX - blackPixels.at(i).x) + (avgY - blackPixels.at(i).y)*(avgY - blackPixels.at(i).y));
+            }
+            avgLen = avgLen/blackPixels.size();
+
+            std::cout << "Len: " << (openLen - avgLen)/openLen << "\t";
+        }
+        else
+            std::cout << "EMPTY!" << "\t";
+
         //std::cout << avgCenter.x << "," << avgCenter.y << "\t";
-        std::cout << avgMass/area << "\t" << mass/area << "\t";
+        //std::cout << avgMass/area << "\t" << mass/area << "\t";
         std::cout << (avgMass/area - mass/area)/(avgMass/area) << "\t";
 
         if((avgMass/area - mass/area)/(avgMass/area) > 0.8)
